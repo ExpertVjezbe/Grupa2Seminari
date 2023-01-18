f65848acee62a17e9fd17b2d22b9913d2d922b78
VEDRAN <VEDRAN@DESKTOP-7FONVHN>
2023-01-18 19:52:08 +0100

    hgh

---
+++ Expert.VedranMarin.Seminar/Controllers/SeminarController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expert.VedranMarin.Seminar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeminarController : ControllerBase
+    {
+    }
+}
