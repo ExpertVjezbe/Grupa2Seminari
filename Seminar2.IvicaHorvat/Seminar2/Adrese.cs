aa893fb5c21d810e93015a1bad6a54ae6ab512f6
exper <exper@gmail.com>
2023-02-04 16:32:21 +0100

    seminar2 predan

---
+++ Seminar2.IvicaHorvat/Seminar2/Adrese.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Seminar2.IvicaHorvat.Seminar2;
+
+public partial class Adrese
+{
+    public int Id { get; set; }
+
+    public string Naziv { get; set; } = null!;
+
+    public string KucniBroj { get; set; } = null!;
+
+    public string? Kat { get; set; }
+
+    public int? IdGrada { get; set; }
+
+    public virtual Gradovi? IdGradaNavigation { get; set; }
+}
