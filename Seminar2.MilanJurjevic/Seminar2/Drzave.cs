23a2f4cec23ce05c0073a7be6fb34c3221cc464a
Milan <milanjurjevic@hotmail.com>
2023-02-07 23:40:47 +0100

    spremno za upload

---
+++ Seminar2.MilanJurjevic/Seminar2/Drzave.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Seminar2.MilanJurjevic.Seminar2;
+
+public partial class Drzave
+{
+    public int Id { get; set; }
+
+    public string Naziv { get; set; } = null!;
+
+    public string Kratica { get; set; } = null!;
+
+    public string? NazivEngleski { get; set; }
+
+    public string? NazivKineski { get; set; }
+
+    public virtual ICollection<Gradovi> Gradovis { get; } = new List<Gradovi>();
+}
